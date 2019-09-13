25e64a1cf05d8d9b88821cf90885ce0cf673cd42
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-13 17:21:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -10,8 +10,8 @@ end
 
 
 #def square_array(array)
-  numbers = [2,3,4]
-  square_array.each do |numbers|**2
-    return new_array = "#{numbers**2}"
-  end
-end
+#  numbers = [2,3,4]
+#  square_array.each do |numbers|**2
+#    return new_array = "#{numbers**2}"
+#  end
+#end
